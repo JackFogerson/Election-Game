2a03b5e356006318225d1042d6e2da0979e2f65e
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2020-02-02 17:10:01 -0800

    Add files via upload

--- src/Events.java
+++ src/Events.java
@@ -1,12 +1,14 @@
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class Events {	
 	Statistics s;
+	JFrame eventFrame;
 	
 	public Events(String c, Statistics stat) {
 		this.s = stat;
@@ -16,10 +18,10 @@ public class Events {
 	}
 	
 	public void johnsonEventChain1() {
-		JFrame johnsonFrame = new JFrame("Gary Johnson");
-		johnsonFrame.setLayout(new GridLayout(5, 1));
-		
-		JTextField q = new JTextField("Choose your VP");
+		eventFrame = new JFrame("Gary Johnson");
+		eventFrame.setLayout(new GridLayout(5, 1));
+				
+		JTextField q = new JTextField("Choose your VP:");
 		JButton bw = new JButton("Bill Weld       ");
 		JButton ap = new JButton("Austin Peterson ");
 		JButton jm = new JButton("John Macafee    ");
@@ -30,42 +32,56 @@ public class Events {
 		jm.addActionListener(event -> je1_3());
 		vs.addActionListener(event -> je1_4());
 		
-		johnsonFrame.add(bw);
-		johnsonFrame.add(ap);
-		johnsonFrame.add(jm);
-		johnsonFrame.add(vs);
+		q.setEditable(false);
+		ImageIcon bwp = new ImageIcon("BW_Photo.jpg");
+		ImageIcon app = new ImageIcon("AP_Photo.jpg");
+		ImageIcon jmp = new ImageIcon("JM_Photo.jpg");
+
+		bw.setIcon(bwp);
+		ap.setIcon(app);
+		jm.setIcon(jmp);
+
+		eventFrame.add(q);
+		eventFrame.add(bw);
+		eventFrame.add(ap);
+		eventFrame.add(jm);
+		eventFrame.add(vs);
 		
-		johnsonFrame.setSize(new Dimension(950, 500));
+		eventFrame.setSize(new Dimension(1000, 500));
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		//Set JFrame to center of screen
-		johnsonFrame.setLocation(d.width/2-johnsonFrame.getSize().width/2, d.height/2-johnsonFrame.getSize().height/2);
-		johnsonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		johnsonFrame.setVisible(true);	
+		eventFrame.setLocation(d.width/2-eventFrame.getSize().width/2, d.height/2-eventFrame.getSize().height/2);
+		eventFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		eventFrame.setVisible(true);	
 	}
 	
 	public void je1_1() {
 		s.Massachusetts.changeLib(.04);
+		eventFrame.dispose();
 		johnsonEventChain2();
 	}
 	
 	public void je1_2() {
 		s.Missouri.changeLib(.01);
+		eventFrame.dispose();
 		johnsonEventChain2();
 	}
 	
 	public void je1_3() {
 		s.California.changeLib(.02);
+		eventFrame.dispose();
 		johnsonEventChain2();
 	}
 	
 	public void je1_4() {
 		s.Alaska.changeLib(.09);
+		eventFrame.dispose();
 		johnsonEventChain2();
 	}
 	
 	public void johnsonEventChain2() {
-		JFrame johnsonFrame = new JFrame("Gary Johnson");
-		johnsonFrame.setLayout(new GridLayout(5, 1));
+		eventFrame = new JFrame("Gary Johnson");
+		eventFrame.setLayout(new GridLayout(5, 1));
 		
 		JTextField q = new JTextField("Allepo!");
 		JButton fu = new JButton("...And what is Allepo?");
@@ -78,36 +94,40 @@ public class Events {
 		sp.addActionListener(event -> je2_3(s));
 		aj.addActionListener(event -> je2_4(s));
 		
-		johnsonFrame.add(fu);
-		johnsonFrame.add(nc);
-		johnsonFrame.add(sp);
-		johnsonFrame.add(aj);
+		eventFrame.add(fu);
+		eventFrame.add(nc);
+		eventFrame.add(sp);
+		eventFrame.add(aj);
 		
-		johnsonFrame.setSize(new Dimension(950, 500));
+		eventFrame.setSize(new Dimension(950, 500));
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		//Set JFrame to center of screen
-		johnsonFrame.setLocation(d.width/2-johnsonFrame.getSize().width/2, d.height/2-johnsonFrame.getSize().height/2);
-		johnsonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		johnsonFrame.setVisible(true);	
+		eventFrame.setLocation(d.width/2-eventFrame.getSize().width/2, d.height/2-eventFrame.getSize().height/2);
+		eventFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		eventFrame.setVisible(true);	
 	}
 	
 	public void je2_1(Statistics s) {
 		s.Massachusetts.changeLib(.04);
+		eventFrame.dispose();
 		new ResultsPage(s);
 	}
 	
 	public void je2_2(Statistics s) {
 		s.Missouri.changeLib(.01);
+		eventFrame.dispose();
 		new ResultsPage(s);
 	}
 	
 	public void je2_3(Statistics s) {
 		s.California.changeLib(.02);
+		eventFrame.dispose();
 		new ResultsPage(s);
 	}
 	
 	public void je2_4(Statistics s) {
 		s.Alaska.changeLib(.09);
+		eventFrame.dispose();
 		new ResultsPage(s);
 	}
 }
